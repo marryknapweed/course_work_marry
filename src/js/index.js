b1ffc863bb565a0f13ed87ed5273889e060f6e4b
marryknapweed <mariakostyukova88@gmail.com>
2024-01-06 23:51:49 +0300

    save section slider completed and layout section main no media

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,54 @@
+// import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
+
+// let right = document.querySelectorAll(".slider-block__right");
+
+// right.forEach((elem, idx) => {
+//   if (idx % 2 == 0) {
+//     elem.classList.add("bg_1");
+//   } else {
+//     elem.classList.add("bg_2");
+//   }
+// });
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: "vertical",
+  direction: "horizontal",
   loop: true,
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  speed: 2000,
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
@@ -15,7 +56,17 @@ const swiper = new Swiper(".swiper", {
   },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
+
+let right = document.querySelectorAll(".slider-block__right");
+
+right.forEach((elem, idx) => {
+  if (idx % 2 == 0) {
+    elem.classList.add("bg_1");
+  } else {
+    elem.classList.add("bg_2");
+  }
 });
